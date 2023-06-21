a75fcaae660bba5e94dcdb70b10e833c768876e7
analuizalinda <135667510+analuizalinda@users.noreply.github.com>
2023-06-21 19:46:42 -0300

    Add files via upload

---
+++ aula_bd_48.sql
@@ -0,0 +1,75 @@
+create database empresa_xpto;
+
+use empresa_xpto;
+
+create table funcionarios(
+id int auto_increment,
+nome varchar(60),
+sobrenome varchar(60),
+salario decimal (10,2),
+primary key(id)
+);
+
+load data infile 'c:/ProgramData/MySQL/MySQL server 8.0/uploads/funcionarios.csv'
+into table funcionarios
+fields terminated by','
+lines terminated by '\n'
+ignore 1 lines 
+(nome, sobrenome, salario) set id = null;
+
+select * from funcionarios;
+
+select * from funcionarios where salario <1238.11;
+
+ select nome,sobrenome from funcionarios order by salario limit 5;
+ 
+ select nome,sobrenome from funcionarios order by salario desc limit 5;
+ 
+ select * from funcionarios 
+ where nome = "Gil" or nome = "Carlo";
+  
+  select * from funcionarios 
+  where (nome = "Gil" or nome = "Carlo") and salario > 15000;
+  
+  select * from funcionarios 
+  where sobrenome = 'Benn' or sobrenome = 'Ramet';
+  
+  select * from funcionarios 
+  where (sobrenome = 'Benn' or sobrenome = 'Ramet') and salario <5000;
+  
+  select * from funcionarios 
+  where salario Between 5000 and 10000;
+  
+  select nome,sobrenome from funcionarios
+  where salario between 2000 and 3000;
+  
+  select sum(salario) as soma_salarios 
+  from funcionarios;
+  
+  select avg(salario) as media_salarios 
+  from funcionarios;
+  
+  select count(*) as total_salarios 
+  from funcionarios;
+ 
+ select max(salario) as maior_salario
+ from funcionarios;
+ 
+ select min(salario) as maior_salario 
+ from funcionarios;
+ 
+ select count(*) as total 
+ from funcionarios where salario > 15000;
+ 
+ select count(*) as total
+ from funcionarios where salario between 1212 and 5000;
+ 
+ select count(*) as total 
+ from funcionarios where salario between 5001 and 10000;
+ 
+ select count(*) as total 
+ from funcionarios where salario between 10001 and 15000;
+ 
+ 
+
+ 
